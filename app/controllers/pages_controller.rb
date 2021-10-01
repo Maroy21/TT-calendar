ff5a54fad1499290469bee904e9cc45607ff9912
marat <Persidskii222@ya.ru>
2021-10-01 15:37:03 +0300

    added button next

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
+  
   def main
     @user = current_user
+    @date = Date.today
+    
   end
+
+  def show
+    
+    @date = Date.new(params[:year].to_i, ((params[:month].to_i - 1) % 12) +1, 1)
+    
+    
+  end
+
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#main'
+  get '/:year/:month', to: 'pages#show'
   
   devise_for :users
 end
